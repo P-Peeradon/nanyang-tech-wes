f84e9bf6c4900629eba315c589944ca6787a924c
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-11-12 00:02:28 +1100

    Creating BAckend Services

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
-import './assets/style.css' // Or './style.css'
+import App from './App.vue';
+import router from './router';
+import './assets/style.css'; // Or './style.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    },
+});
 
-app.mount('#app')
+app.mount('#app');
