11a2f3f7f913086493d29620c9c5ae9b3d0c88a0
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-11-22 18:13:02 +1100

    Preparing loginview

---
+++ client/src/layouts/BlankLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <WebHeader />
+        <slot />
+        <WebFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+import WebHeader from '../components/WebHeader.vue';
+import WebFooter from '../components/WebFooter.vue';
+</script>
+
+<style scoped>
+
+</style>
