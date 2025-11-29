e0e9300d52ac0243c00dc1480c21190f6f724172
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-11-29 21:46:24 +1100

    Add getCourses route.

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 // FIX: Change 'require' to 'import' statements
 import { fetchFeatures, loginUser, registerUser } from '../controller/SessionController.js'; 
 import { getUser } from '../controller/UserController.js'
+import { getCourses } from '../controller/RegistrationController.js';
 import { authenticateToken } from '../middleware/middleware.js';
 
 const router = express.Router();
@@ -11,5 +12,7 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 router.get('/user/:userId', authenticateToken, getUser);
+
+router.get('/course', getCourses);
 
 export default router;
