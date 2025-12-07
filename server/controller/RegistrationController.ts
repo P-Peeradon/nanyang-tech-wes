e1459ccf8215e3821a0bb88a256351796501ebae
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-12-07 19:22:09 +1100

    Add method to delete enrolment.

--- server/controller/RegistrationController.ts
+++ server/controller/RegistrationController.ts
@@ -66,11 +66,13 @@ export const getStudentEnrolments = async (req: Request, res: Response, next: Ne
     }
 }
 
-
+// Create new enrolment for a student in NTU
+// POST /enrolment
+// Protected
 export const createEnrolment = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { studentId, courseCode } = req.body;
-        const { user } = (req as any).user;
+        const user: object = (req as any).user ?? {};
 
         if (!user) {
             return next(new HttpError('You are unauthorised to add enrolment data.', 401))
@@ -80,21 +82,58 @@ export const createEnrolment = async (req: Request, res: Response, next: NextFun
             return next(new HttpError('Please enter your student ID and course code', 400));
         }
 
-        if (user?.id !== studentId) {
-            return next(new HttpError('You cannot enrol courses on behalf of other student'));
+        /*
+        if (user.id !== studentId) {
+            return next(new HttpError('You cannot enrol courses on behalf of other student', 403));
         }
+        */
 
         // Add to enrolment table.
-        const query: string = 'INSERT INTO enrolment (std_id, cos_code, enrol_date, enrol_year, enrol_semester) VALUES (?, ?, ?, ?, ?)'
-        await pool.execute(query, [studentId, courseCode, new Date(), 2025, 1]);
+        const query: string = 'INSERT INTO enrolment (std_id, cos_code, enrol_year, enrol_semester) VALUES (?, ?, ?, ?)'
+        await pool.execute(query, [studentId, courseCode, 2025, 1]);
+        await pool.commit();
 
         // But send 202 as the status is not approved.
         return res.status(202).json({
             studentId, 
             courseCode, 
-            status: 'pending', 
-            remark: ''
+            year: 2025,
+            semester: 1,
         });
+    } catch (err: any) {
+        return next(new HttpError(err));
+    }
+}
+
+// Create new enrolment for a student in NTU
+// DELETE /enrolment
+// Protected
+export const deleteEnrolment = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { studentId, courseCode } = req.body;
+        const user: object = (req as any).user ?? {};
+
+        if (!user) {
+            return next(new HttpError('You are unauthorised to delete enrolment data.', 401))
+        }
+
+        if (!studentId || !courseCode) {
+            return next(new HttpError('Please enter your student ID and course code', 400));
+        }
+
+        /*
+        if (user.id !== studentId) {
+            return next(new HttpError('You cannot enrol courses on behalf of other student', 403));
+        }
+        */
+
+        // Add to enrolment table.
+        const query: string = 'DELETE FROM enrolment WHERE std_id = ? AND cos_code = ?'
+        await pool.execute(query, [studentId, courseCode]);
+        await pool.commit();
+
+        // But send 204 as we do not need any data, but if successful, that data must be wiped out from the state.
+        return res.status(204)
     } catch (err: any) {
         return next(new HttpError(err));
     }
