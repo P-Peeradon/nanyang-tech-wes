877ab775b2dad7bc490d63c3d42cf350bc585047
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-12-08 13:38:44 +1100

    Get data from student state.

--- client/src/display/ProfileDisplay.vue
+++ client/src/display/ProfileDisplay.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-auto px-2">
         <div class="flex flex-row gap-10">
-            <ProfileCard :name="'Stu1'" :program="'Bachelor of X'" :id="'NTU00000'" />
+            <ProfileCard :name="studentState.studentData?.fullName ?? ''" :program="studentState.studentData?.program ?? ''" :id="studentState.studentData?.studentId ?? NanyangID" />
             <EventCalendar />
         </div>
         <br />
@@ -13,6 +13,10 @@
 import ProfileCard from '../components/ProfileCard.vue';
 import AcademicProfileCard from '../components/AcademicProfileCard.vue';
 import EventCalendar from '../components/EventCalendar.vue';
+import { studentStore } from '../stores/profile';
+
+const studentState = studentStore();
+const NanyangID: string = localStorage.getItem('studentId') ?? '';
 </script>
 
 <style></style>
