87397af28b3820bbe3fb7c1276377909f7eda222
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-12-11 21:09:29 +1100

    Fixing database connection for GCP

--- server/db/database.ts
+++ server/db/database.ts
@@ -7,6 +7,7 @@ const access: PoolOptions = {
     user: process.env.MYSQL_USER ?? '',
     password: process.env.MYSQL_PASSWORD ?? '',
     database: process.env.MYSQL_DATABASE ?? '',
+    socketPath: process.env.MYSQL_SOCKET ?? ''
 };
 
 // 2. Create the Connection Pool
